a0f3cb500a0b4fc6fa5210153341a7117052b1c2
hyunseo11 <70734028+hyunseo11@users.noreply.github.com>
2024-03-14 11:34:40 +0900

    4주차 피드백 반영

--- signin/signin.js
+++ signin/signin.js
@@ -1,8 +1,17 @@
+/*에러메세지가 떠 있는 경우 다시 focus했을 때 에러메세지 제거*/
+function removeErrorMessage() {
+  const emailErrorMessageEl = document.querySelector('.error-message-email');
+  const passwordErrorMessageEl = document.querySelector('.error-message-password');
+
+  emailErrorMessageEl.textContent = '';
+  passwordErrorMessageEl.textContent = '';
+}
+
 /*email 유효성 검증*/
 
-function validateEmail() {
+function validateEmail(event) {
 
-  const emailInputEl = document.querySelector('.email-input');
+  const emailInputEl = event.target;
   const emailValue = emailInputEl.value;
 
   const errorMessageEl = document.querySelector('.error-message-email');
@@ -14,21 +23,20 @@ function validateEmail() {
   } else if(!emailValue.includes("@")) {
     errorMessageEl.textContent = "올바른 이메일 주소가 아닙니다";
     emailInputEl.classList.add('wrongsign');
-
-  } else {
-    errorMessageEl.textContent = "";
-    emailInputEl.classList.remove('wrongsign');
   }
+
+  emailInputEl.classList.remove('wrongsign');
 }
 
 const emailInputEl = document.querySelector('.email-input');
 emailInputEl.addEventListener('focusout', validateEmail);
+emailInputEl.addEventListener('focus', removeErrorMessage);
 
 
 /*password 유효성 검증*/
 
-function validatePassword() {
-  const passwordInputEl = document.querySelector('.password-input');
+function validatePassword(event) {
+  const passwordInputEl = event.target;
   const passwordValue = passwordInputEl.value;
 
   const errorMessageEl = document.querySelector('.error-message-password');
@@ -36,20 +44,26 @@ function validatePassword() {
   if(passwordValue === '') {
     errorMessageEl.textContent = "비밀번호를 입력해주세요";
     passwordInputEl.classList.add('wrongsign');
-
-  } else {
-    errorMessageEl.textContent = "";
-    passwordInputEl.classList.remove('wrongsign');
   }
+
+    passwordInputEl.classList.remove('wrongsign');
+
 }
 
 const passwordInputEl = document.querySelector('.password-input');
 passwordInputEl.addEventListener('focusout', validatePassword);
+passwordInputEl.addEventListener('focus', removeErrorMessage);
 
-/*이메일: test@codeit.com 비밀번호: codeti101로 로그인 시, /folder 페이지로 이동
+/*이메일: test@codeit.com 비밀번호: codeit101의 사용자 등록*/
+  const users = [
+    {email: "test@codeit.com", password: "codeit101"}
+  ];
+
+
+/*이메일: test@codeit.com 비밀번호: codeit101로 로그인 시, /folder 페이지로 이동
 이외의 로그인 시도의 경우, 에러 메세지 출력*/
 
-function validateForm(event) {
+function handleFormSubmit(event) {
   event.preventDefault();
 
   const emailValue = emailInputEl.value;
@@ -58,23 +72,34 @@ function validateForm(event) {
   const errorMessageEl_email = document.querySelector('.error-message-email');
   const errorMessageEl_password = document.querySelector('.error-message-password');
 
-  if (emailValue === "test@codeit.com" && passwordValue === "codeit101") {
-    location.href = "./folder.html";
-
-  } else {
+  const useremail = users.find(user => user.email === emailValue);
+  const userpassword = users.find(user => user.password === passwordValue);
+  
+  if(!useremail) {
     errorMessageEl_email.textContent = "이메일을 확인해주세요";
+  } else {
+    errorMessageEl_email.textContent = "";
+  }
+  
+  if(!userpassword) {
     errorMessageEl_password.textContent = "비밀번호를 확인해주세요";
+  } else {
+    errorMessageEl_password.textContent = "";
+  }
+
+  if(useremail && userpassword) {
+    location.href = "./folder.html";
   }
 }
 
 const signform = document.querySelector('.sign-form');
-signform.addEventListener('submit', validateForm);
+signform.addEventListener('submit', handleFormSubmit);
 
 /*Enter키를 눌러도 로그인이 되도록 추가*/
 
 signform.addEventListener('keydown', (event) => {
   if(event.key === "Enter") {
-    validateForm.submit();
+    handleFormSubmit.submit();
   }
 });
 
