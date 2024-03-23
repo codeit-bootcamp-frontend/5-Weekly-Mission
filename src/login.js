e68741584b86eca29d07224071e730ffdd651652
수연 <sy@csy-MacBook.local>
2024-03-24 00:31:35 +0900

    5주차 과제 수정

--- src/login.js
+++ src/login.js
@@ -1,50 +1,68 @@
 import {
-    inputError,
-    removeInputError,
-    emailPattern,
-    USER_INFO
-} from "./module.js"
+  inputError,
+  removeInputError,
+  emailPattern,
+  togglePw,
+  USER_INFO,
+} from "./module.js";
 
 const emailInput = document.querySelector("#email");
 const emailErrorMsg = document.querySelector("#email-error");
-emailInput.addEventListener('focusout', validateEmail());
 function validateEmail(email) {
-    if(email === "") {
-        inputError({input: emailInput, errorMsg: emailErrorMsg}, "이메일을 입력해주세요");
-        return;
-    }
-    if (!emailPattern(email)) {
-        inputError({input: emailInput, errorMsg: emailErrorMsg}, "올바른 이메일 주소가 아닙니다");
-        return;
-    }
-    removeInputError({input: emailInput, errorMsg: emailErrorMsg});
+  if (email === "") {
+    inputError(
+      { input: emailInput, errorMsg: emailErrorMsg },
+      "이메일을 입력해주세요"
+    );
+    return;
+  }
+  if (!emailPattern(email)) {
+    inputError(
+      { input: emailInput, errorMsg: emailErrorMsg },
+      "올바른 이메일 주소가 아닙니다"
+    );
+    return;
+  }
+  removeInputError({ input: emailInput, errorMsg: emailErrorMsg });
 }
+emailInput.addEventListener("focusout", (e) => validateEmail(e.target.value));
 
-
-
-const pwInput = document.querySelector("#paswword");
+const pwInput = document.querySelector("#password");
 const pwErrorMsg = document.querySelector("#pw-error");
-pwInput.addEventListener("focusout", (event) => validatePw(event.target.value));
 function validatePw(password) {
-    if (password === "") {
-        inputError({input:pwInput, errorMsg: pwErrorMsg}, "비밀번호를 입력해주세요");
-        return;
-    }
-    removeInputError({input:pwInput, errorMsg: pwErrorMsg});
+  if (password === "") {
+    inputError(
+      { input: pwInput, errorMsg: pwErrorMsg },
+      "비밀번호를 입력해주세요"
+    );
+    return;
+  }
+  removeInputError({ input: pwInput, errorMsg: pwErrorMsg });
 }
+pwInput.addEventListener("focusout", (event) => validatePw(event.target.value));
 
-const signForm = document.querySelector("#sign-form");
+const pwToggleBtn = document.querySelector("#pw-toggle");
+pwToggleBtn.addEventListener("click", () => togglePw(pwInput, pwToggleBtn));
+
+const signForm = document.querySelector("#form");
 signForm.addEventListener("submit", submitForm);
-function submitForm(event) {
-  event.preventDefault();
+function submitForm(e) {
+  e.preventDefault();
 
   const testUser =
-    emailInput.value === USER_INFO.email && pwInput.value === USER_INFO.password;
+    emailInput.value === USER_INFO.email &&
+    pwInput.value === USER_INFO.password;
 
   if (testUser) {
     location.href = "/folder";
     return;
   }
-  inputError({ input: emailInput, errorMsg: emailErrorMsg }, "이메일을 확인해주세요.");
-  inputError({ input: passwordInput, errorMsg: pwErrorMsg }, "비밀번호를 확인해주세요.");
+  inputError(
+    { input: emailInput, errorMsg: emailErrorMsg },
+    "이메일을 확인해주세요."
+  );
+  inputError(
+    { input: pwInput, errorMsg: pwErrorMsg },
+    "비밀번호를 확인해주세요."
+  );
 }
